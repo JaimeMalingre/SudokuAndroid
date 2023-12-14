39e24626f32c131a03a84f101a71ebc822799546
Jaime Malingre <jaimemalingre@gmail.com>
2023-12-14 14:58:01 +0100

    Sudoku

---
+++ app/src/main/java/com/example/sudokuandroid/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.sudokuandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); // Set the content view to the activity_main.xml file
+
+    }
+}
